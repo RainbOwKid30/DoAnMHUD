064aaab513d8b1ec589b2690d7e36dca95c9bbf4
RainbOwKid30 <hauem471@gmail.com>
2023-10-04 20:31:10 +0700

    update do an 4/10

---
+++ DoAnMaHoaChuDon.py
@@ -0,0 +1,88 @@
+import random
+import tkinter as tk
+
+class BangMaChuDon:
+    def __init__(self, key):
+        self.key = key
+        self.alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+    def encrypt(self, plaintext):
+        plaintext = plaintext.lower()
+        ciphertext = ''
+        for char in plaintext:
+            if char in self.alphabet:
+                index = self.alphabet.index(char)
+                ciphertext += self.key[index]
+            else:
+                ciphertext += char
+        return ciphertext
+
+    def decrypt(self, ciphertext):
+        plaintext = ''
+        for char in ciphertext:
+            if char in self.key:
+                index = self.key.index(char)
+                plaintext += self.alphabet[index]
+            else:
+                plaintext += char
+        return plaintext
+
+def generate_random_key():
+    alphabet_list = list('abcdefghijklmnopqrstuvwxyz')
+    random.shuffle(alphabet_list)
+    return ''.join(alphabet_list)
+
+def encrypt_text():
+    plaintext = plaintext_entry.get().lower()
+    encrypted_text = cipher.encrypt(plaintext)
+    encrypted_text_label.config(text=f'Encrypted: {encrypted_text}')
+
+def decrypt_text():
+    ciphertext = ciphertext_entry.get().lower()
+    decrypted_text = cipher.decrypt(ciphertext)
+    decrypted_text_label.config(text=f'Decrypted: {decrypted_text}')
+
+def generate_new_random_key():
+    global cipher
+    random_key = generate_random_key()
+    cipher = BangMaChuDon(random_key)
+    random_key_label.config(text=f'Random Key: {random_key}')
+
+# Khởi tạo giao diện người dùng
+root = tk.Tk()
+root.title("Bang Ma Chu Don")
+
+# Tạo bảng mã chữ đơn với khóa ngẫu nhiên
+random_key = generate_random_key()
+cipher = BangMaChuDon(random_key)
+
+# Tạo và định cấu hình các phần tử giao diện người dùng
+plaintext_label = tk.Label(root, text="Plaintext:")
+plaintext_label.pack()
+plaintext_entry = tk.Entry(root)
+plaintext_entry.pack()
+
+encrypt_button = tk.Button(root, text="Encrypt", command=encrypt_text)
+encrypt_button.pack()
+
+ciphertext_label = tk.Label(root, text="Ciphertext:")
+ciphertext_label.pack()
+ciphertext_entry = tk.Entry(root)
+ciphertext_entry.pack()
+
+decrypt_button = tk.Button(root, text="Decrypt", command=decrypt_text)
+decrypt_button.pack()
+
+random_key_button = tk.Button(root, text="Random Key", command=generate_new_random_key)
+random_key_button.pack()
+
+random_key_label = tk.Label(root, text=f'Random Key: {random_key}')
+random_key_label.pack()
+
+encrypted_text_label = tk.Label(root, text="")
+encrypted_text_label.pack()
+
+decrypted_text_label = tk.Label(root, text="")
+decrypted_text_label.pack()
+
+root.mainloop()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,34 @@
-    keyPublic = random.randint()
-    print(keyPublic)
+
+def encrypt(text, key): 
+    numColum = len(key)
+    numRows = int(len(text) / numColum)
+    if len(text) % len(key) !=0:
+        numRows+=1
+    mt = []
+    a = 0
+    for i in range(numRows):
+        for j in range(numColum):
+            if a >= len(text):
+                mt[i,j] = 'z'
+                a+=1
+            else:
+                mt[i,j] = text.replace('', a)
+                a+=1
+    key_as_array = []
+    for char in key:
+        key_as_array.append(char)
+    String_char = []
+    mt.sort(key_as_array)
+    for item in key_as_array:
+        numkey = key.find()
+        for i in range(numRows):
+            String_char.append(mt[i,numkey])
+    return String_char
+
+text = input("Nhap van ban cho no: ")
+key = input("nhap key cho no:")
+print(encrypt(text,key))
+
+
+    
+                
